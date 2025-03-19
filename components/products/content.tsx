16dcef5dc608c0415c28db8e07c96f20595ca022
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2025-03-19 03:11:07 -0300

    feat ( ui ) ajuste de responsividade

--- components/products/content.tsx
+++ components/products/content.tsx
@@ -6,7 +6,7 @@ import { FC } from "react"
 
 export const Content: FC<ProductTableProps> = ({products}) => {
     return(
-        <div className="flex min-h-screen p-4 lg:p-0">
+        <div className="min-h-screen p-4 lg:p-0 lg:flex">
             <Sidebar />
             <main className="flex-1 p-4">
                 <h2 className="text-2xl font-bold mb-4">Lista de Produtos</h2>
