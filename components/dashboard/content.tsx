ab2b25c1d659e7f26f24f0772292ecca4dd651a5
MarcelinoRodrigues <marcelino.rodrigues.d.n@gmail.com>
2025-04-11 00:22:58 -0300

    feat - migrando dashboard para server component

--- components/dashboard/content.tsx
+++ components/dashboard/content.tsx
@@ -1,51 +1,31 @@
-"use client"
+import { Windownnw } from "./windonnw";
+import { Fragment } from "react";
+import { getDashboard } from "@/services/dashboard/getDashboard";
 
-import { Windownnw } from "./windonnw"
-import { Fragment, useCallback, useEffect, useState } from "react"
-import { getDashboard } from "@/services/dashboard/getDashboard"
-import { Loader2 } from "lucide-react"
-import { DashboardItem } from "@/types/dashboard/types"
+type DashboardData = {
+  products: number;
+  sales: number;
+  stocks: number;
+};
 
-export const Content = () => {
-  const [dashboard, setDashboard] = useState<DashboardItem[]>([])
-  const [isLoading, setIsLoading] = useState(true)
-
-  const enableLoading = useCallback(() => setIsLoading(true), []);
-  const disableLoading = useCallback(() => setIsLoading(false), []);
-
-  useEffect(() => {
-    const fetchDashboard = async () => {
-      enableLoading();
-      try {
-        const response = await getDashboard();
-        setDashboard(response || []);
-      } finally {
-        disableLoading();
-      }
-    };
-
-    fetchDashboard();
-  }, [enableLoading, disableLoading]);
+export const Content = async () => {
+  const dashboard: DashboardData[] = await getDashboard();
 
   return (
-    <div className="mx-auto border-8 border-gray-900 p-4 h-screen overflow-auto min-w-[50vw]">
-      <h2 className="bg-gray-900 text-white rounded-2xl text-center">Dashboard</h2>
-      {isLoading && (
-        <div className="flex items-center justify-center h-[80%]">
-          <Loader2 size={45} className="animate-spin text-white" />
-        </div>
-      )}
-      {!isLoading && dashboard.length > 0 && (
-        <div className="flex justify-between">
-          {dashboard.map((item, index) => (
-            <Fragment key={index}>
-              <Windownnw title="Produtos" quantity={item.products} />
-              <Windownnw title="Vendas" quantity={item.sales} />
-              <Windownnw title="Estoque" quantity={item.stocks} />
-            </Fragment>
-          ))}
-        </div>
-      )}
+    <div className="mx-auto max-w-6xl px-4 py-6 w-full">
+      <h2 className="text-3xl font-bold text-center text-white bg-gray-900 py-3 rounded-xl mb-6 shadow">
+        Dashboard
+      </h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {dashboard.map((item: DashboardData, index) => (
+          <Fragment key={index}>
+            <Windownnw title="Produtos" quantity={item.products} />
+            <Windownnw title="Vendas" quantity={item.sales} />
+            <Windownnw title="Estoque" quantity={item.stocks} />
+          </Fragment>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
