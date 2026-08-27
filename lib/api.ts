2bc7aa26e128ec21b5a278be0c969459deb754a1
MarcelinoRodrigues <marcelino.rodrigues.d.n@gmail.com>
2026-08-26 23:01:45 -0300

    Change API_BASE_URL environment variable source

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import https from "https";
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "";
+export const API_BASE_URL = process.env.API_BASE_URL_ENV || "";
 
 export const agent = new https.Agent({
   rejectUnauthorized: false,
